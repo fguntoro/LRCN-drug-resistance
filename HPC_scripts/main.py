92a8f5eb1db237607c02ccf18eea468337b7adff
fguntoro <fernando_guntoro@hotmail.com>
2021-05-10 13:16:05 +0100

    additional comments and notebook
    some comments on functions and notebook for running functions and some output from first run

---
+++ HPC_scripts/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import sklearn.model_selection as model_selection
+
+# pipe_line_gene.py
+from loading_data import data_preprocess
+from models import Bayesian_optimizer, model_gene_based, wide_n_deep, ML_methods, deepAMR_run
+from feature_importance import base_approach, lime
+from data_analyzer import source_analysis
+from dataset_creator import gene_dataset_creator
+
+df = pd.read_csv("Data/AllLabels.csv")
+f = pd.read_csv("Data/gene_data.csv")
+df_train, labels = data_preprocess.process(38, gene_dataset=True)
+df_instudy = df[df["id"].isin(df_train.index.values)]
+df_instudy.sum(axis = 0, skipna = True)
--- pipe_line_gene.py
+++ pipe_line_gene.py
@@ -9,18 +9,18 @@ epochs = 200
 
 def train():
     # deepAMR_run.main()
-    df_train, labels = data_preprocess.process(38, gene_dataset=True)
+    df_train, labels = data_preprocess.process(38, gene_dataset=True) # SNP or GENE
     # gene_dataset_creator.main()
     # df_train, labels = data_preprocess.process(38)
     # source_analysis.main(df_train)
     # df_train, labels = data_preprocess.process(38, shuffle_operon_group=True)
-    # ML_methods.model_run(df_train, labels)
+    # ML_methods.model_run(df_train, labels) # BO for ML and GBT
     # model_gene_based.run_model(df_train, labels, epochs)
-    # wide_n_deep.run_bayesian(df_train, labels)
-    # model_gene_based.run_bayesian(df_train, labels)
+    # wide_n_deep.run_bayesian(df_train, labels) # BO for wide n deep method
+    # model_gene_based.run_bayesian(df_train, labels) # main diff in data folding nested CV # init_point 15, n_iter 15 # run_one_fold also run_k_fold and run_single_fold (section.3.5 help accuracy)
     model_gene_based.run_bayesian_single(df_train, labels)
-    # model_gene_based.run_all(df_train, labels, epochs)
-    # model_gene_based.run_model_kfold(df_train,labels,epochs)
+    model_gene_based.run_all(df_train, labels, epochs)
+    model_gene_based.run_model_kfold(df_train,labels,epochs)
 
 
     # base_approach.run_feature_importance(df_train, labels)
