8afc1a777f5497782028138a6cfc73a9605e4cf8
fguntoro <fernando_guntoro@hotmail.com>
2021-05-17 11:02:08 +0100

    put function into main() and included timer

--- HPC_scripts/main.py
+++ HPC_scripts/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import sklearn.model_selection as model_selection
+import time
 
 # pipe_line_gene.py
 from loading_data import data_preprocess
@@ -9,6 +10,16 @@ from feature_importance import base_approach, lime
 from data_analyzer import source_analysis
 from dataset_creator import gene_dataset_creator
 
-df = pd.read_csv("Data/AllLabels.csv")
-f = pd.read_csv("Data/gene_data.csv")
-df_train, labels = data_preprocess.process(38, gene_dataset=True)
+def main():
+    """Running the LRCN model on Imperial HPC"""
+    tic = time.perf_counter()
+
+    df = pd.read_csv("Data/AllLabels.csv")
+    f = pd.read_csv("Data/gene_data.csv")
+    df_train, labels = data_preprocess.process(38, gene_dataset=True)
+
+    toc = time.perf_counter()
+    print(f"Analysis was run in {toc - tic:0.4f} seconds")
+
+if __name__ == "__main__":
+    main()
